128bbd47d2d184c77b3fe85fe613cb5ea2311b97
Amanda Ferreira <amandaaraujoferreiraa@gmail.com>
2025-06-15 14:34:24 -0300

    Alterações feitas

--- src/main/java/com/medcenter/Medcenter/config/CorsConfig.java
+++ src/main/java/com/medcenter/Medcenter/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // Coloca o IP do seu front aqui
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
